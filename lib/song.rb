8d63f60744b046f9dec1f0b9ea5c377794e7abda
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-04 19:53:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require_relative './concerns/memorable.rb'
 class Song
-  extend Memorable::ClassMethods
+  extend Memorable#::ClassMethods
   attr_accessor :name
   attr_reader :artist
 
